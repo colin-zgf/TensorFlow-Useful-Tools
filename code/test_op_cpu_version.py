92009924bbc477110761ed548d2695632144dbd3
colin-zgf <54894807+colin-zgf@users.noreply.github.com>
2019-10-12 15:51:26 +0800

    Delete test_op_cpu_version.py

--- code/test_op_cpu_version.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-so_file = '/usr/local/lib/python3.5/dist-packages/tensorflow/core/user_ops/my_add.so'
-op_module = tf.load_op_library(so_file)
-
-with tf.Session(''):
-    x = op_module.my_add([6, 4], [2, 4]).eval()
-print(x)
